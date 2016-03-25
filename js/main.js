8fc6acb5a646f298feaafd8b487673484e5b3ffe
nisiyu <nisiyu91@gmail.com>
2016-03-25 21:41:46 +0800

    add several radio buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 /**
  * Created by siyu on 2016/3/20.
  */
+$(".cvss").click(function(){
+alert(1);
+});
+
+$("input").click(function(){
+    var name = this.name;
+    $("input[name=\"" + name + "\"]").css("background-color","white");
+    $(this).css("background-color","#ccc");
+});
