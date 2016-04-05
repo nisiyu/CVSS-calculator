a61c4c86dada23dc3cdb71d2aa86a0b7f10664b6
nisiyu <nisiyu91@gmail.com>
2016-04-06 00:05:34 +0800

    add explanations

--- js/main.js
+++ js/main.js
@@ -102,8 +102,28 @@ $("input").click(function(){
         }
         return 4;
     };
-    
-    $("div.rectangle").animate({height:percentage + "%", backgroundColor: segcolor[seglevel(cvss)]},slow);
+
+    $("div.rectangle").animate({height:percentage + "%", backgroundColor: segcolor[seglevel(cvss)]});
     $("div.rectangle h4").text(cvss);
 });
 
+var resourcesdict={
+    AV: "This metric reflects the context by which vulnerability exploitation is possible. This metric value (and consequently the Base score) will be larger the more remote (logically, and physically) an attacker can be in order to exploit the vulnerable component. The assumption is that the number of potential attackers for a vulnerability that could be exploited from across the Internet is larger than the number of potential attackers that could exploit a vulnerability requiring physical access to a device, and therefore warrants a greater score. The list of possible values is presented in Table 1.",
+    AC: "This metric describes the conditions beyond the attacker's control that must exist in order to exploit the vulnerability. As described below, such conditions may require the collection of more information about the target, the presence of certain system configuration settings, or computational exceptions. Importantly, the assessment of this metric excludes any requirements for user interaction in order to exploit the vulnerability (such conditions are captured in the User Interaction metric). This metric value is largest for the least complex attacks. The list of possible values is presented in Table 2.",
+    PR: "This metric describes the level of privileges an attacker must possess before successfully exploiting the vulnerability. This metric is greatest if no privileges are required. The list of possible values is presented in Table 3.",
+    UI: "This metric captures the requirement for a user, other than the attacker, to participate in the successful compromise of the vulnerable component. This metric determines whether the vulnerability can be exploited solely at the will of the attacker, or whether a separate user (or user-initiated process) must participate in some manner. This metric value is greatest when no user interaction is required. The list of possible values is presented in Table 4.",
+    S: "Intuitively, one may think of a scope change as breaking out of a sandbox, and an example would be a vulnerability in a virtual machine that enables an attacker to delete files on the host OS (perhaps even its own VM). In this example, there are two separate authorization authorities: one that defines and enforces privileges for the virtual machine and its users, and one that defines and enforces privileges for the host system within which the virtual machine runs. A scope change would not occur, for example, with a vulnerability in Microsoft Word that allows an attacker to compromise all system files of the host OS, because the same authority enforces privileges of the user's instance of Word, and the host's system files.",
+    C: "This metric measures the impact to the confidentiality of the information resources managed by a software component due to a successfully exploited vulnerability. Confidentiality refers to limiting information access and disclosure to only authorized users, as well as preventing access by, or disclosure to, unauthorized ones. The list of possible values is presented in Table 6. This metric value increases with the degree of loss to the impacted component.",
+    I: "This metric measures the impact to integrity of a successfully exploited vulnerability. Integrity refers to the trustworthiness and veracity of information. The list of possible values is presented in Table 7. This metric value increases with the consequence to the impacted component.",
+    A: "This metric measures the impact to the availability of the impacted component resulting from a successfully exploited vulnerability. While the Confidentiality and Integrity impact metrics apply to the loss of confidentiality or integrity of data (e.g., information, files) used by the impacted component, this metric refers to the loss of availability of the impacted component itself, such as a networked service (e.g., web, database, email). Since availability refers to the accessibility of information resources, attacks that consume network bandwidth, processor cycles, or disk space all impact the availability of an impacted component. The list of possible values is presented in Table 8. This metric value increases with the consequence to the impacted component."
+};
+
+$(".exploit-metrics").hover(function(){
+    var elem = this.getElementsByTagName("input")[0];
+    var metrics = elem.getAttribute("name");
+    $(".explanation>h2").text(metrics);
+    $(".explanation>p").text(resourcesdict[metrics]);
+},function(){
+    $(".explanation>h2").text("");
+    $(".explanation>p").text("");
+});
